fc735c288e7f79d62a8b40e6f3b095c1632aba28
Cosmo Coder <madhavtiwari2024@gmail.com>
2026-03-28 14:10:48 +0530

    Changed the image on the landing page on the cash flow analysis section

--- landing-page/src/features/homepage/use-cases/UseCases.tsx
+++ landing-page/src/features/homepage/use-cases/UseCases.tsx
@@ -7,7 +7,7 @@ import { Progress } from "@/components/Progress";
 import { TypebotLogoFull } from "@/components/TypebotLogo";
 import { registerUrl } from "@/constants";
 import marketingBotSrc from "./assets/marketing-bot.png";
-import marketingBuilderSrc from "./assets/marketing-builder.png";
+import marketingBuilderSrc from "./assets/uix.png";
 import salesBotSrc from "./assets/sales-bot.png";
 import salesBuilderSrc from "./assets/sales-builder.png";
 import supportAndProductBotSrc from "./assets/support-bot.png";
